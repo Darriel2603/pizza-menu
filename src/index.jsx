87794536c55e50cc02c884981406369f58638117
darriel2603 <darrielmarkerizal@gmail.com>
2023-08-12 14:17:04 +0700

    first commit

---
+++ src/index.jsx
@@ -0,0 +1,110 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+const pizzaData = [
+  {
+    name: "Pizza Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
+
+function App() {
+  return (
+    <div className="max-w-screen-xl mx-auto flex flex-col items-center gap-12">
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Pizza Time CO.</h1>
+    </header>
+  );
+}
+
+function Menu() {
+  return (
+    <main className="menu">
+      <h2>Our Menu</h2>
+      <Pizza />
+    </main>
+  );
+}
+
+function Footer() {
+  const getHour = new Date().getHours();
+  const openHour = 8;
+  const closeHour = 22;
+
+  const isOpen = openHour <= getHour && getHour < closeHour;
+
+  return (
+    <div className="footer">
+      <footer>
+        {isOpen
+          ? "We're open until 22:00 PM. Come visit us or order online"
+          : "We're closed, Come back tomorrow at 8:00 AM"}
+      </footer>
+    </div>
+  );
+}
+
+function Pizza() {
+  return (
+    <div className="pizza">
+      <img src="pizzas/focaccia.jpg" alt="Pizza Foccacia" />
+      <h3>"Pizza Foccacia"</h3>
+      <p>"Bread with italian olive oil and rosemary"</p>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
