dcae33196382be985078c4355034ffc6608c47a4
Darriel2603 <darrielmarkerizal@gmail.com>
2023-08-12 19:08:48 +0700

    selesai

--- src/index.jsx
+++ src/index.jsx
@@ -146,16 +146,16 @@ function Order({ closeHour, openHour }) {
 }
 
 function Pizza({ pizzaObj }) {
-  if (pizzaObj.soldOut) return null;
+  // if (pizzaObj.soldOut) return null;
   console.log(pizzaObj);
 
   return (
-    <li className="pizza">
+    <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
       <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
         <h3>{pizzaObj.name}</h3>
         <p>{pizzaObj.ingredients}</p>
-        <span>{pizzaObj.price}</span>
+        <span>{pizzaObj.soldOut ? "SOLD OUT" : pizzaObj.price}</span>
       </div>
     </li>
   );
